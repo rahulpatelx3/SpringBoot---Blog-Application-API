dc78e487c7866dbc222e81bc42348ebda4c40df0
Rahul Patel <rahulpatelx3@gmail.com>
2022-04-23 12:22:55 +0530

    first commit

---
+++ src/main/java/com/blog/api/BlogApplicationApiApplication.java
@@ -0,0 +1,13 @@
+package com.blog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplicationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplicationApiApplication.class, args);
+	}
+
+}
